42bf45943ed7e9e67dea6af5480ec5f32254a9b2
Pyae Phyo Aung <learncloudhub@gmail.com>
2026-09-11 14:58:45 +0630

    ;rgb:bfbf/bfbf/bfbfrgb:0000/0000/0000

--- calculator.py
+++ calculator.py
@@ -1,8 +1,7 @@
+# A super simple 1-line calculation tool
 expr = input("Enter math expression (e.g., 2+3*5): ")
 try:
     print("Result:", eval(expr))
 except:
     print("Invalid math expression.")
-
-print("Updated")
 
